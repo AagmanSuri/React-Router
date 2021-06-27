33bca4d171e54dd3aefcf32dfea04afc837a4013
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-06-27 11:26:50 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>React Router </h1>
       <h2>Edit to see some magic happen!</h2>
     </div>
   );
