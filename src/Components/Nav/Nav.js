259d46e056012015e162e68553075c33536b90cf
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-06-27 15:03:31 +0530

    nav update Link react-router-dom

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,12 +1,18 @@
 import "./Nav.css";
 
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   return (
     <div>
       <ul className="Nav">
         <h3>Logo</h3>
-        <li className="NavLinks">Shop</li>
-        <li className="NavLinks">About</li>
+        <Link className="NavLinks" to="/Shop">
+          <li>Shop</li>
+        </Link>
+        <Link className="NavLinks" to="/About">
+          <li>About</li>
+        </Link>
       </ul>
     </div>
   );
