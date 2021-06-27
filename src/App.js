a4d8f1e85f80f8f27fab4b5fa4de93e3b7b4edf9
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-06-27 14:28:07 +0530

    Router, Route path defining

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Nav from "./Components/Nav/Nav";
 import About from "./Components/About";
 import Shop from "./Components/Shop";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 export default function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Home />
-      <Shop />
-      <About />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        {/* < Route path =Home /> */}
+        {/* Here path is defined to create a route */}
+        <Route path="/Shop" component={Shop} />
+        <Route path="/About" component={About} />
+      </div>
+    </Router>
   );
 }
